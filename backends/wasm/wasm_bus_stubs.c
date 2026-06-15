b3b9bea79f688851c009166936a1079f55093b22
Kamil Lulko <kamil.lulko@gmail.com>
2026-06-16 00:55:46 +0200

    backends/wasm: fix bus stub timeout types + add async HAL stubs

--- backends/wasm/wasm_bus_stubs.c
+++ backends/wasm/wasm_bus_stubs.c
@@ -30,13 +30,14 @@ void ove_hal_uart_close(ove_uart_t u)
 {
 	(void)u;
 }
-int ove_hal_uart_tx(ove_uart_t u, const void *d, size_t l, uint32_t t, size_t *w)
+int ove_hal_uart_tx(ove_uart_t uart, const void *data, size_t len, uint64_t timeout_ns,
+		    size_t *bytes_written)
 {
-	(void)u;
-	(void)d;
-	(void)l;
-	(void)t;
-	(void)w;
+	(void)uart;
+	(void)data;
+	(void)len;
+	(void)timeout_ns;
+	(void)bytes_written;
 	return OVE_ERR_NOT_SUPPORTED;
 }
 int ove_hal_uart_rx_enable(ove_uart_t u)
@@ -64,13 +65,21 @@ void ove_hal_spi_close(ove_spi_t s)
 {
 	(void)s;
 }
-int ove_hal_spi_transfer(ove_spi_t s, const void *tx, void *rx, size_t l, uint32_t t)
+int ove_hal_spi_transfer(ove_spi_t spi, const void *tx, void *rx, size_t len, uint64_t timeout_ns)
 {
-	(void)s;
+	(void)spi;
 	(void)tx;
 	(void)rx;
-	(void)l;
-	(void)t;
+	(void)len;
+	(void)timeout_ns;
+	return OVE_ERR_NOT_SUPPORTED;
+}
+int ove_hal_spi_transfer_async(ove_spi_t spi, const void *tx, void *rx, size_t len)
+{
+	(void)spi;
+	(void)tx;
+	(void)rx;
+	(void)len;
 	return OVE_ERR_NOT_SUPPORTED;
 }
 #endif
@@ -88,34 +97,46 @@ void ove_hal_i2c_close(ove_i2c_t i)
 {
 	(void)i;
 }
-int ove_hal_i2c_write(ove_i2c_t i, uint16_t a, const void *d, size_t l, uint32_t t)
+int ove_hal_i2c_write(ove_i2c_t i2c, uint16_t addr, const void *data, size_t len,
+		      uint64_t timeout_ns)
 {
-	(void)i;
-	(void)a;
-	(void)d;
-	(void)l;
-	(void)t;
+	(void)i2c;
+	(void)addr;
+	(void)data;
+	(void)len;
+	(void)timeout_ns;
 	return OVE_ERR_NOT_SUPPORTED;
 }
-int ove_hal_i2c_read(ove_i2c_t i, uint16_t a, void *b, size_t l, uint32_t t)
+int ove_hal_i2c_read(ove_i2c_t i2c, uint16_t addr, void *buf, size_t len, uint64_t timeout_ns)
 {
-	(void)i;
-	(void)a;
-	(void)b;
-	(void)l;
-	(void)t;
+	(void)i2c;
+	(void)addr;
+	(void)buf;
+	(void)len;
+	(void)timeout_ns;
 	return OVE_ERR_NOT_SUPPORTED;
 }
-int ove_hal_i2c_write_read(ove_i2c_t i, uint16_t a, const void *tx, size_t tl, void *rx, size_t rl,
-			   uint32_t t)
+int ove_hal_i2c_write_read(ove_i2c_t i2c, uint16_t addr, const void *tx, size_t tx_len, void *rx,
+			   size_t rx_len, uint64_t timeout_ns)
 {
-	(void)i;
-	(void)a;
+	(void)i2c;
+	(void)addr;
 	(void)tx;
-	(void)tl;
+	(void)tx_len;
 	(void)rx;
-	(void)rl;
-	(void)t;
+	(void)rx_len;
+	(void)timeout_ns;
+	return OVE_ERR_NOT_SUPPORTED;
+}
+int ove_hal_i2c_write_read_async(ove_i2c_t i2c, uint16_t addr, const void *tx, size_t tx_len,
+				 void *rx, size_t rx_len)
+{
+	(void)i2c;
+	(void)addr;
+	(void)tx;
+	(void)tx_len;
+	(void)rx;
+	(void)rx_len;
 	return OVE_ERR_NOT_SUPPORTED;
 }
 #endif
